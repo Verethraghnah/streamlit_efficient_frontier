991e27b39a3a625f9c303584d2721cbe68a28285
Ahmad Mostafavi <82012903+Verethraghnah@users.noreply.github.com>
2022-02-15 20:09:55 +0330

    Update efficient_frontier_streamlit_base.py

--- efficient_frontier_streamlit_base.py
+++ efficient_frontier_streamlit_base.py
@@ -407,6 +407,54 @@ if sidebar_function == "Value at Risk (VaR) analysis":
                         weights = np.array(weights)
                         var = VaR(df)
                         output = var.standard_var(weights)
+                        
+def load_data(ticker):
+    data = yf.download(ticker, START, TODAY)
+    data.reset_index(inplace=True)
+    return data
+
+
+data_load_state = st.text('Loading data...')
+data = load_data(selected_stock)
+data_load_state.text('Loading data... done!')
+
+st.subheader('Raw data')
+st.write(data.tail())
+
+
+# Plot raw data
+def plot_raw_data():
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open"))
+    fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"))
+    fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
+    st.plotly_chart(fig)
+
+
+plot_raw_data()
+
+# Predict forecast with Prophet.
+df_train = data[['Date', 'Close']]
+df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
+
+m = Prophet(seasonality_mode='multiplicative')
+m.add_seasonality(name='monthly', period=30.5, fourier_order=5)
+m.add_country_holidays(country_name='US')
+m.fit(df_train)
+future = m.make_future_dataframe(periods=period)
+forecast = m.predict(future)
+
+# Show and plot forecast
+st.subheader('Forecast data')
+st.write(forecast.tail())
+
+st.write(f'Forecast plot for {n_years} years')
+fig1 = plot_plotly(m, forecast)
+st.plotly_chart(fig1)
+
+st.write("Forecast components")
+fig2 = plot_components_plotly(m, forecast)
+st.write(fig2)
                     
 st.write('Disclaimer: Information and output provided on this site does \
          not constitute investment advice.')
